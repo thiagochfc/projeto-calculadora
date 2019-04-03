1099acb00b035785850772f2f9a28ffcfee192ce
Thiago Christopher <thiago_cfc@hotmail.com>
2019-04-03 18:07:51 -0300

    Ajustes nos arquivos para atender o projeto em camadas

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using projeto_calculadora.View;
+using System;
 using System.Windows.Forms;
 
 namespace projeto_calculadora

39664731882c12b237ed67fe4b3ffb28e51d8710
Thiago Christopher <thiago_cfc@hotmail.com>
2019-04-03 18:47:00 -0300

    Ajustes nos arquivos para atender o projeto em camadas - 2

--- Controller/ControllerPrincipal.cs
+++ Controller/ControllerPrincipal.cs
@@ -10,30 +10,219 @@ namespace projeto_calculadora.Controller
     class ControllerPrincipal
     {
 
+        internal double _NumeroUm { get; set; }
+        internal double _NumeroDois { get; set; }
+        internal string _Operacao { get; set; }
+        internal bool _PressionouIgual { get; set; }
+        internal double _Resultado { get; set; }
+
+        // Limpa todos os campos
+        internal void LimparCampos()
+        {
+            _NumeroUm = 0;
+            _NumeroDois = 0;
+            _Operacao = string.Empty;
+            _PressionouIgual = false;
+        }
+
+        // Limpa o TxtResultado
+        internal void LimparTxtResultado(TextBox txt)
+        {
+            txt.Clear();
+        }
+
+        // Verifica se Txt é igual a zer
         internal bool VerificaSeIgualZero(TextBox txt)
         {
             if (txt.Text.Trim().Equals("0")) return true;
             else return false;
         }
 
+        // Verifica se Txt está vázio
         internal bool VerificaSeVazio(TextBox txt)
         {
             if (txt.Text.Trim().Equals(string.Empty)) return true;
             else return false;
         }
 
-
+        // Verifica se tem . no Txt
         internal bool VerificaSeTemPonto(TextBox txt)
         {
             if (txt.Text.Trim().Contains(".")) return true;
             else return false;
         }
 
+        // Verifica se o igual foi pressionado
         internal bool VeriricaSeIgualPressionado(bool pressionouIgual)
         {
             if (pressionouIgual) return true;
             else return false;
         }
 
+        // Calcula o Resultado
+        internal void CalcularResultado(string operacao, TextBox txt)
+        {
+            switch (operacao)
+            {
+                case "/":
+                if (_NumeroDois == 0)
+                {
+                    MessageBox.Show("Não é permitido divisão por 0", "Calculadora", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                }
+                else
+                    _Resultado = _NumeroUm / _NumeroDois;
+                break;
+
+                case "*":
+                _Resultado = _NumeroUm * _NumeroDois;
+                break;
+
+                case "-":
+                _Resultado = _NumeroUm - _NumeroDois;
+                break;
+
+                case "+":
+                _Resultado = _NumeroUm + _NumeroDois;
+                break;
+
+                case "^":
+                _Resultado = CalcularPotencia(_NumeroUm, _NumeroDois);
+                break;
+
+            }
+            txt.Text = _Resultado.ToString().Replace(",", ".");
+        }
+
+        // Insere o valor
+        internal void InserirValor(string valor, TextBox txt)
+        {
+            if (VeriricaSeIgualPressionado(_PressionouIgual))
+            {
+                LimparTxtResultado(txt);
+                _PressionouIgual = false;
+            }
+            if (VerificaSeIgualZero(txt)) txt.Text += valor;
+            else txt.Text += valor;
+        }
+
+        // Adiciona a operação
+        internal void AdicionarOperacao(string operacao, TextBox txt)
+        {
+            if (!VerificaSeVazio(txt))
+            {
+                if (VerificaSeTemPonto(txt)) _NumeroUm = Convert.ToDouble(txt.Text.Trim().Replace(".", ","));
+                else _NumeroUm = Convert.ToDouble(txt.Text.Trim());
+                _Operacao = operacao;
+                LimparTxtResultado(txt);
+            }
+            else
+            {
+                MessageBox.Show("Precisar ser inserido algum valor", "Calculadora", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // Calcula a potência
+        private double CalcularPotencia(double valorBase, double valorExpoente)
+        {
+            return Math.Pow(valorBase, valorExpoente);
+
+        }
+
+        // Ação quando o botão igual é pressionado
+        internal void ActionIgual(TextBox txt)
+        {
+            if (_PressionouIgual)
+            {
+                LimparTxtResultado(txt);
+                _PressionouIgual = false;
+                return;
+            }
+            if (!VerificaSeVazio(txt))
+            {
+
+                if (VerificaSeTemPonto(txt)) _NumeroDois = Convert.ToDouble(txt.Text.Trim().Replace(".", ","));
+                else _NumeroDois = Convert.ToDouble(txt.Text.Trim());
+                CalcularResultado(_Operacao, txt);
+                _PressionouIgual = true;
+            }
+        }
+
+        // Ação qundo o botão . é pressionado
+        internal void ActionPonto(TextBox txt)
+        {
+            if (_PressionouIgual)
+            {
+                txt.Text = "0.";
+                _PressionouIgual = false;
+                return;
+            }
+            if (VerificaSeVazio(txt)) txt.Text = "0.";
+            if (VerificaSeTemPonto(txt)) return;
+            txt.Text += ".";
+        }
+
+        // Ação quando o botão <-- é pressionado
+        internal void ActionLimpaUltimoValor(TextBox txt)
+        {
+            if (_Operacao.Equals(string.Empty) || _PressionouIgual) LimparCampos();
+            else LimparTxtResultado(txt);
+
+        }
+
+        // Ação quando o botão TrocaSinal é pressionado
+        internal void ActionTrocaSinal(TextBox txt)
+        {
+            if (!VerificaSeVazio(txt)) txt.Text = (Convert.ToDouble(txt.Text.Trim().Replace(".", ",")) * (-1)).ToString().Replace(",", ".");
+        }
+
+        // Ação quando o botão LimpaUltimo é pressionado
+        internal void ActionRemoveUltimo(int tamanho, TextBox txt)
+        {
+            string Texto = txt.Text.Trim();
+            LimparTxtResultado(txt);
+            for (int i = 0; i < tamanho - 1; i++) txt.Text += Texto[i];
+        }
+
+        // Ação quando o botão ElevadoQuadrado é pressionado
+        internal void ActionElevaQuadrado(TextBox txt)
+        {
+            if (!VerificaSeVazio(txt))
+            {
+                double ValorBase = Convert.ToDouble(txt.Text.Trim().Replace(".", "."));
+                double Resultado = CalcularPotencia(ValorBase, 2);
+                txt.Text = Resultado.ToString().Replace(",", ".");
+                _PressionouIgual = true;
+            }
+        }
+
+        // Ação quando o botão Raiz é pressionado
+        internal void ActionRaiz(TextBox txt)
+        {
+            if (!VerificaSeVazio(txt))
+            {
+                double ValorBase = Convert.ToDouble(txt.Text.Trim().Replace(".", "."));
+                double Resultado = Math.Sqrt(ValorBase);
+                txt.Text = Resultado.ToString().Replace(",", ".");
+                _PressionouIgual = true;
+            }
+        }
+
+        // Ação quando o botão 1x é pressionado
+        internal void Action1x(TextBox txt)
+        {
+            if (!VerificaSeVazio(txt))
+            {
+                double ValorBase = Convert.ToDouble(txt.Text.Trim().Replace(".", "."));
+                if (ValorBase == 0)
+                {
+                    MessageBox.Show("Erro divisão por zero", "Calculadora", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                double Resultado = 1 / ValorBase;
+                txt.Text = Resultado.ToString().Replace(",", ".");
+                _PressionouIgual = true;
+            }
+        }
     }
 }
--- View/FrmPrincipal.cs
+++ View/FrmPrincipal.cs
@@ -9,105 +9,19 @@ namespace projeto_calculadora.View
 
         ControllerPrincipal controller = new ControllerPrincipal();
 
-        private double _NumeroUm;
-        private double _NumeroDois;
-        private string _Operacao;
-        private bool _PressionouIgual;
-        private double _Resultado;
-
         public FrmPrincipal()
         {
             InitializeComponent();
             PnlFundo.Focus();
         }
 
-        private void LimparTxtResultado()
-        {
-            TxtResultado.Clear();
-        }
-
-        private void LimparCampos()
-        {
-            LimparTxtResultado();
-            _NumeroUm = 0;
-            _NumeroDois = 0;
-            _Operacao = string.Empty;
-            _PressionouIgual = false;
-            PnlFundo.Focus();
-        }
+        private void LimparTxtResultado() => controller.LimparTxtResultado(TxtResultado);
 
         private void FrmPrincipal_Load(object sender, EventArgs e)
         {
-            LimparCampos();
+            LimparTxtResultado();
+            controller.LimparCampos();
             PnlFundo.Focus();
-        }
-
-        private void CalcularResultado()
-        {
-            switch (_Operacao)
-            {
-                case "/":
-                if (_NumeroDois == 0)
-                {
-                    MessageBox.Show("Não é permitido divisão por 0", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    break;
-                }
-                else
-                    _Resultado = _NumeroUm / _NumeroDois;
-                break;
-
-                case "*":
-                _Resultado = _NumeroUm * _NumeroDois;
-                break;
-
-                case "-":
-                _Resultado = _NumeroUm - _NumeroDois;
-                break;
-
-                case "+":
-                _Resultado = _NumeroUm + _NumeroDois;
-                break;
-
-                case "^":
-                _Resultado = CalcularPotencia(_NumeroUm, _NumeroDois);
-                break;
-
-            }
-            TxtResultado.Text = _Resultado.ToString().Replace(",", ".");
-        }
-
-        private void InserirValor(string valor)
-        {
-            if (controller.VeriricaSeIgualPressionado(_PressionouIgual))
-            {
-                LimparTxtResultado();
-                _PressionouIgual = false;
-            }
-            if (controller.VerificaSeIgualZero(TxtResultado)) TxtResultado.Text += valor;
-            else TxtResultado.Text += valor;
-            PnlFundo.Focus();
-        }
-
-        private void AdicionarOperacao(string operacao)
-        {
-            if (!controller.VerificaSeVazio(TxtResultado))
-            {
-                if (controller.VerificaSeTemPonto(TxtResultado)) _NumeroUm = Convert.ToDouble(TxtResultado.Text.Trim().Replace(".", ","));
-                else _NumeroUm = Convert.ToDouble(TxtResultado.Text.Trim());
-                _Operacao = operacao;
-                LimparTxtResultado();
-            }
-            else
-            {
-                MessageBox.Show("Precisar ser inserido algum valor", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            PnlFundo.Focus();
-        }
-
-        public double CalcularPotencia(double valorBase, double valorExpoente)
-        {
-            return Math.Pow(valorBase, valorExpoente);
-
         }
 
         private void BtnZero_Click(object sender, EventArgs e)
@@ -115,175 +29,87 @@ namespace projeto_calculadora.View
             if (!controller.VerificaSeIgualZero(TxtResultado)) TxtResultado.Text += "0";
         }
 
-        private void BtnUm_Click(object sender, EventArgs e)
-        {
-            InserirValor("1");
-        }
+        private void BtnUm_Click(object sender, EventArgs e) => controller.InserirValor("1", TxtResultado);
 
-        private void BtnDois_Click(object sender, EventArgs e)
-        {
-            InserirValor("2");
-        }
+        private void BtnDois_Click(object sender, EventArgs e) => controller.InserirValor("2", TxtResultado);
 
-        private void BtnTres_Click(object sender, EventArgs e)
-        {
-            InserirValor("3");
-        }
+        private void BtnTres_Click(object sender, EventArgs e) => controller.InserirValor("3", TxtResultado);
 
-        private void BtnQuatro_Click(object sender, EventArgs e)
-        {
-            InserirValor("4");
-        }
+        private void BtnQuatro_Click(object sender, EventArgs e) => controller.InserirValor("4", TxtResultado);
+        
+        private void BtnCinco_Click(object sender, EventArgs e) => controller.InserirValor("5", TxtResultado);
 
-        private void BtnCinco_Click(object sender, EventArgs e)
-        {
-            InserirValor("5");
-        }
+        private void BtnSeis_Click(object sender, EventArgs e) => controller.InserirValor("6", TxtResultado);
 
-        private void BtnSeis_Click(object sender, EventArgs e)
-        {
-            InserirValor("6");
-        }
+        private void BtnSete_Click(object sender, EventArgs e) => controller.InserirValor("7", TxtResultado);
 
-        private void BtnSete_Click(object sender, EventArgs e)
-        {
-            InserirValor("7");
-        }
+        private void BtnOito_Click(object sender, EventArgs e) => controller.InserirValor("8", TxtResultado);
 
-        private void BtnOito_Click(object sender, EventArgs e)
-        {
-            InserirValor("8");
-        }
+        private void BtnNove_Click(object sender, EventArgs e) => controller.InserirValor("9", TxtResultado);
 
-        private void BtnNove_Click(object sender, EventArgs e)
-        {
-            InserirValor("9");
-        }
+        private void BtnDividir_Click(object sender, EventArgs e) => controller.AdicionarOperacao("/", TxtResultado);
 
-        private void BtnDividir_Click(object sender, EventArgs e)
-        {
-            AdicionarOperacao("/");
-        }
+        private void BtnMultiplicar_Click(object sender, EventArgs e) => controller.AdicionarOperacao("*", TxtResultado);
 
-        private void BtnMultiplicar_Click(object sender, EventArgs e)
-        {
-            AdicionarOperacao("*");
-        }
+        private void BtnSubtrair_Click(object sender, EventArgs e) => controller.AdicionarOperacao("-", TxtResultado);
 
-        private void BtnSubtrair_Click(object sender, EventArgs e)
-        {
-            AdicionarOperacao("-");
-        }
-
-        private void BtnSomar_Click(object sender, EventArgs e)
-        {
-            AdicionarOperacao("+");
-        }
+        private void BtnSomar_Click(object sender, EventArgs e) => controller.AdicionarOperacao("+", TxtResultado);
 
         private void BtnIgual_Click(object sender, EventArgs e)
         {
-            if (_PressionouIgual)
-            {
-                LimparTxtResultado();
-                _PressionouIgual = false;
-                return;
-            }
-            if (!controller.VerificaSeVazio(TxtResultado))
-            {
-
-                if (controller.VerificaSeTemPonto(TxtResultado)) _NumeroDois = Convert.ToDouble(TxtResultado.Text.Trim().Replace(".", ","));
-                else _NumeroDois = Convert.ToDouble(TxtResultado.Text.Trim());
-                CalcularResultado();
-                _PressionouIgual = true;
-            }
+            controller.ActionIgual(TxtResultado);
             PnlFundo.Focus();
         }
 
         private void BtnPonto_Click(object sender, EventArgs e)
         {
-            if (_PressionouIgual)
-            {
-                TxtResultado.Text = "0.";
-                _PressionouIgual = false;
-                return;
-            }
-            if (controller.VerificaSeVazio(TxtResultado)) TxtResultado.Text = "0.";
-            if (controller.VerificaSeTemPonto(TxtResultado)) return;
-            TxtResultado.Text += ".";
+            controller.ActionPonto(TxtResultado);
             PnlFundo.Focus();
         }
 
         private void BtnLimpar_Click(object sender, EventArgs e)
         {
-            LimparCampos();
+            controller.LimparCampos();
+            LimparTxtResultado();
             PnlFundo.Focus();
         }
 
         private void BtnLimpaUltimoValor_Click(object sender, EventArgs e)
         {
-            if (_Operacao.Equals(string.Empty) || _PressionouIgual) LimparCampos();
-            else LimparTxtResultado();
+            controller.ActionLimpaUltimoValor(TxtResultado);
             PnlFundo.Focus();
         }
 
         private void BtnTrocaSinal_Click(object sender, EventArgs e)
         {
-            if (!controller.VerificaSeVazio(TxtResultado)) TxtResultado.Text = (Convert.ToDouble(TxtResultado.Text.Trim().Replace(".", ",")) * (-1)).ToString().Replace(",", ".");
+            controller.ActionTrocaSinal(TxtResultado);
             PnlFundo.Focus();
         }
 
         private void BtnRemoveUltimo_Click(object sender, EventArgs e)
         {
             int Tamanho = TxtResultado.Text.Trim().Length;
-            string Texto = TxtResultado.Text.Trim();
-            LimparTxtResultado();
-            for (int i = 0; i < Tamanho - 1; i++) TxtResultado.Text += Texto[i];
+            controller.ActionRemoveUltimo(Tamanho, TxtResultado);
             PnlFundo.Focus();
         }
 
         private void BtnElevadoQuadrado_Click(object sender, EventArgs e)
         {
-            if (!controller.VerificaSeVazio(TxtResultado))
-            {
-                double ValorBase = Convert.ToDouble(TxtResultado.Text.Trim().Replace(".", "."));
-                double Resultado = CalcularPotencia(ValorBase, 2);
-                TxtResultado.Text = Resultado.ToString().Replace(",", ".");
-                _PressionouIgual = true;
-            }
+            controller.ActionElevaQuadrado(TxtResultado);
             PnlFundo.Focus();
         }
 
-        private void BtnPotencia_Click(object sender, EventArgs e)
-        {
-            AdicionarOperacao("^");
-        }
+        private void BtnPotencia_Click(object sender, EventArgs e) => controller.AdicionarOperacao("^", TxtResultado);
 
         private void BtnRaiz_Click(object sender, EventArgs e)
         {
-            if (!controller.VerificaSeVazio(TxtResultado))
-            {
-                double ValorBase = Convert.ToDouble(TxtResultado.Text.Trim().Replace(".", "."));
-                double Resultado = Math.Sqrt(ValorBase);
-                TxtResultado.Text = Resultado.ToString().Replace(",", ".");
-                _PressionouIgual = true;
-            }
+            controller.ActionRaiz(TxtResultado);
             PnlFundo.Focus();
         }
 
         private void Btn1x_Click(object sender, EventArgs e)
         {
-            if (!controller.VerificaSeVazio(TxtResultado))
-            {
-                double ValorBase = Convert.ToDouble(TxtResultado.Text.Trim().Replace(".", "."));
-                if (ValorBase == 0)
-                {
-                    MessageBox.Show("Erro divisão por zero", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-                double Resultado = 1 / ValorBase;
-                TxtResultado.Text = Resultado.ToString().Replace(",", ".");
-                _PressionouIgual = true;
-            }
+            controller.Action1x(TxtResultado);
             PnlFundo.Focus();
         }
 
